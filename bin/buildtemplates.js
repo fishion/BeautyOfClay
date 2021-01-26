ec4f5c48a6d00c32cb27edc6285fd1f7008c8f63
Alex <alex@monney.me>
2021-01-26 23:53:49 +0000

    put wrapper into the templates directly

--- bin/buildtemplates.js
+++ bin/buildtemplates.js
@@ -8,17 +8,17 @@ require('marko/node-require').install({compilerOptions: {
 const templatesDir = `${__dirname}/../templates`;
 const renderedDir = `${__dirname}/../docs`;
 
-
-const wrapper = require(`${templatesDir}/partials/wrapper`);
 const templates = fs.readdirSync(templatesDir, {withFileTypes: true})
   .filter(item => !item.isDirectory())
   .map(item => item.name.replace(/\.marko$/, ""));
 
 for (let i=0; i<templates.length; i++){
-  console.log(`generating ${templates[i]}`)
-  const renderedPage = require(`${templatesDir}/${templates[i]}`).renderToString();
-  var writeStream = fs.createWriteStream(`${renderedDir}/${templates[i]}.html`);
-  wrapper.stream({ content: renderedPage }).pipe(writeStream)
-  //writeStream.end();
+  const pagename = templates[i];
+  console.log(`generating ${pagename}`)
+  const page = require(`${templatesDir}/${pagename}`);
+  var writeStream = fs.createWriteStream(`${renderedDir}/${pagename}.html`);
+  page.stream({
+    [`page_${pagename}`] : true
+  }).pipe(writeStream)
   console.log('done');
 }
