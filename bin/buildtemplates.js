16dabcd6e8226b564a050bcac2e400e43477cddf
Alex <alex@monney.me>
2021-01-27 19:03:34 +0000

    marko can't render css files. Annoying

--- bin/buildtemplates.js
+++ bin/buildtemplates.js
@@ -5,32 +5,38 @@ const fs = require('fs');
 require('marko/node-require').install({compilerOptions: {
   writeToDisk: false
 }});
+
+// set constants
 const templatesDir = `${__dirname}/../templates`;
 const renderedDir = `${__dirname}/../docs`;
 const slideshowImagesDir = `${__dirname}/../docs/img/slideshow`;
+const secondsPerImage = 6;
 
-
+// read file lists
 const templates = fs.readdirSync(templatesDir, {withFileTypes: true})
   .filter(item => !item.isDirectory())
   .map(item => item.name.replace(/\.marko$/, ""));
-
-  
 const slideshowImages = fs.readdirSync(slideshowImagesDir, {withFileTypes: true})
   .filter(item => !item.isDirectory())
   .map(item => `../img/slideshow/${item.name}`)
 
-
-
-for (let i=0; i<templates.length; i++){
-  const pagename = templates[i];
+const generate = (pagename, extension) => {
   console.log(`generating ${pagename}`)
   const page = require(`${templatesDir}/${pagename}`);
-  var writeStream = fs.createWriteStream(`${renderedDir}/${pagename}.html`);
+  var writeStream = fs.createWriteStream(`${renderedDir}/${pagename}.${extension}`);
   page.stream({
     pagename : {
       [pagename] : true
     },
-    slideshowImages: slideshowImages,
+    slideshow: {
+      images: slideshowImages,
+      secondsPerImage: secondsPerImage
+    }
   }).pipe(writeStream)
   console.log('done');
 }
+
+for (let i=0; i<templates.length; i++){
+  generate(templates[i], 'html');
+}
+generate('css/banner-slideshow-images','css')
