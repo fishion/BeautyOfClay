6813ee3be4c0df6df0f2f4c4cbc06ad9859286c9
Alex <alex@monney.me>
2021-01-30 01:02:24 +0000

    refactor rendering

--- bin/buildtemplates.js
+++ bin/buildtemplates.js
@@ -1,35 +1,15 @@
 #!/usr/bin/env node
 "use strict";
 
-const fs = require('fs');
-var Handlebars = require('handlebars');
-
 // set constants
-const templatesDir = `${__dirname}/../templates`;
+const templatesDir = `${(__dirname)}/../templates`;
 const renderedDir = `${__dirname}/../docs`;
 
-Handlebars.registerHelper("math", (lvalue, operator, rvalue) => {
-  lvalue = parseFloat(lvalue);
-  rvalue = parseFloat(rvalue);
-  return {
-      "+": lvalue + rvalue,
-      "-": lvalue - rvalue,
-      "*": lvalue * rvalue,
-      "/": lvalue / rvalue,
-      "%": lvalue % rvalue
-  }[operator];
-});
+// require modules
+const fs = require('fs');
+const Handlebars = require('../lib/handlebars/enhanced')(templatesDir, renderedDir);
 
-
-Handlebars.registerHelper("wrap", function(wrapper, options) {
-  const compiledWrapper = Handlebars.compile(
-    fs.readFileSync(`${templatesDir}/components/${wrapper}.html.hbs`,'utf8').toString()
-  );
-  return compiledWrapper({ ...this, ...options.hash, content : options.fn(this) })
-});
-
-
-
+// prepare template data
 const slideshowData = {}
 const slideshowImagesDir = `${__dirname}/../docs/img/slideshow`;
 slideshowData.secondsPerImage = 6;
@@ -40,45 +20,8 @@ slideshowData.fullAnimationTime = slideshowData.images.length * slideshowData.se
 slideshowData.keyframePercentPerImage = +(slideshowData.secondsPerImage*100/slideshowData.fullAnimationTime).toFixed(2)
 slideshowData.keyframePercentTransition = +(slideshowData.keyframePercentPerImage/2).toFixed(2)
 
-
-// define render function
-const render = (file) => {
-  // pull apart filepath
-  const fileName = file.replace(new RegExp(`^${templatesDir}/`), "")
-  const fileParts = fileName.split('.'); // myfile.html.hbs
-  const fileOb = {
-    name : fileParts[0],
-    extension : fileParts[1]
-  }
-  console.log(`rendering ${fileOb.name}`)
-  const template = Handlebars.compile(
-    fs.readFileSync(file,'utf8').toString()
-  );
-
-  // render it with data
-  const output = template({
-    pagename : {
-      [fileOb.name] : true
-    },
-    slideshow: slideshowData
-  });
-
-  fs.writeFile(`${renderedDir}/${fileOb.name}.${fileOb.extension}`, output, (err) => {
-    if(err){
-      console.error(err)
-    } else {
-      console.log(`finished rendering ${fileOb.name}`)
-    }
-  })
-
-}
-
-
 // read file lists & render
 const templates = fs.readdirSync(templatesDir, {withFileTypes: true})
   .filter(item => !item.isDirectory())
-  .map(item => `${templatesDir}/${item.name}`);
-for (let i=0; i<templates.length; i++){
-  render(templates[i]);
-}
-render(`${templatesDir}/css/banner-slideshow-images.css.hbs`);
+  .forEach(file => Handlebars.render(file.name, slideshowData))
+Handlebars.render("css/banner-slideshow-images.css.hbs", slideshowData);
