8fc4c5e71209c40d032fd08ce9f5cff44a90a738
Alex <alex@monney.me>
2021-01-30 11:34:10 +0000

    move template data gathering to controllers

--- bin/build.js
+++ bin/build.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 "use strict";
 
-// set constants
+// set paths
 const templatesDir = `${(__dirname)}/../templates`;
 const renderedDir = `${__dirname}/../docs`;
 
@@ -9,19 +9,8 @@ const renderedDir = `${__dirname}/../docs`;
 const fs = require('fs');
 const Handlebars = require('../lib/handlebars/enhanced')(templatesDir, renderedDir);
 
-// prepare template data
-const slideshowData = {}
-const slideshowImagesDir = `${__dirname}/../docs/img/slideshow`;
-slideshowData.secondsPerImage = 6;
-slideshowData.images = fs.readdirSync(slideshowImagesDir, {withFileTypes: true})
-  .filter(item => !item.isDirectory())
-  .map(item => `../img/slideshow/${item.name}`);
-slideshowData.fullAnimationTime = slideshowData.images.length * slideshowData.secondsPerImage,
-slideshowData.keyframePercentPerImage = +(slideshowData.secondsPerImage*100/slideshowData.fullAnimationTime).toFixed(2)
-slideshowData.keyframePercentTransition = +(slideshowData.keyframePercentPerImage/2).toFixed(2)
-
 // read file lists & render
 const templates = fs.readdirSync(templatesDir, {withFileTypes: true})
   .filter(item => !item.isDirectory())
-  .forEach(file => Handlebars.render(file.name, slideshowData))
-Handlebars.render("css/banner-slideshow-images.css.hbs", slideshowData);
+  .forEach(file => Handlebars.render(file.name))
+Handlebars.render("css/banner-slideshow-images.css.hbs", "index");
