554415a2cdac8d06ea2aaf66f50dacd2c293d284
Alex <alex@monney.me>
2021-01-31 20:03:00 +0000

    text tweaks.

--- bin/build.js
+++ bin/build.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 const Handlebars = require('../lib/handlebars/enhanced')(templatesDir, renderedDir);
 
 // read file lists & render
-const templates = fs.readdirSync(templatesDir, {withFileTypes: true})
+fs.readdirSync(templatesDir, {withFileTypes: true})
   .filter(item => !item.isDirectory())
   .forEach(file => Handlebars.render(file.name))
 Handlebars.render("css/banner-slideshow-images.css.hbs", "index");
