205e607e3e7a1190a182535f344b03a194710035
Alex <alex@monney.me>
2021-08-23 19:07:47 +0100

    Use recursive site building in handlebardextended

--- bin/build.js
+++ bin/build.js
@@ -8,15 +8,8 @@ const appRoot = path.resolve(__dirname, '..');
 const config = require(path.resolve(appRoot, 'config.json'));
 
 // build html files with Handlebars
+// require('../../HandlebarsExtended')({
 require('HandlebarsExtended')({
-  ...config.paths,
+    ...config.paths,
   appRoot     : appRoot
 }).buildSite(config);
-
-// one aditional render to do
-require('HandlebarsExtended')({
-  ...config.paths,
-  appRoot         : appRoot,
-  controllerPath  : 'src/controller/css',
-  outputPath      : 'docs/css'
-}).render("../css/banner-slideshow-images.css.hbs", "index");
