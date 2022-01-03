574adcae1b03ee07200e2b27548b5cc2c249c321
Alex <alex@monney.me>
2022-01-03 12:09:21 +0000

    sensible workshop page behaviour when no events

--- src/js/workshops.js
+++ src/js/workshops.js
@@ -22,37 +22,40 @@ const getEvents = async () => {
 
 const displayEvents = (groupedEvents) => {
   Object.keys(groupedEvents).forEach( category => {
-    let eventListHTML = '';
-    Object.values(groupedEvents[category]).forEach(event => {
-      eventListHTML += eventHTML(event)
-    })
     const section = document.getElementById(`${category}-sessions`);
-    section.getElementsByClassName("article-grid")[0].innerHTML = eventListHTML;
-    section.classList.remove('workshop-list-empty');
+    if (Object.keys(groupedEvents[category]).length == 0){
+      section.classList.add('workshop-list-empty');
+    } else {
+      section.getElementsByClassName("article-grid")[0].innerHTML = eventsHTML(groupedEvents[category]);
+    }
+    section.classList.remove('loading');
   })
 }
 
-const eventHTML = (event) => {
-  let html = ''
-  html += '<article>'
-  if (event.img) html += `<img src="${event.img}">`
-  html +=   `<h4>${event.name}</h4>`
-  html +=   `<p>${event.description}</p>`
-  html +=   event.extraContent
-  html +=   `<div class="event-details">`
-  html +=     `<h5>Location : </h5> <p><a href="http://maps.google.com?q=${event.venue.latitude},${event.venue.longitude}" target="new">${event.venue.name}</a></p>`
-  html +=     `<h5>Sessions : </h5> <ul>`
-  for (let i=0; i<event.sessions.length; i++){
-    const session = event.sessions[i];
-    const sTime = `${session.date} | ${session.start} - ${session.end} `;
-    const sPrice = session.price ? session.price : `${session.priceMin} to ${session.priceMax}`
-    html += `<li>${sTime} <a href="${session.url}" target="new" class="button">Book on Eventbrite (${sPrice})</a></li>`
-  }
-  html +=     `</ul>`
-  html +=   `<div>`
-  html += '</article>'
+const eventsHTML = eventList => {
+  let html = '';
+  Object.values(eventList).forEach(event => {
+    html += '<article>'
+    if (event.img) html += `<img src="${event.img}">`
+    html +=   `<h4>${event.name}</h4>`
+    html +=   `<p>${event.description}</p>`
+    html +=   event.extraContent
+    html +=   `<div class="event-details">`
+    html +=     `<h5>Location : </h5> <p><a href="http://maps.google.com?q=${event.venue.latitude},${event.venue.longitude}" target="new">${event.venue.name}</a></p>`
+    html +=     `<h5>Sessions : </h5> <ul>`
+    for (let i=0; i<event.sessions.length; i++){
+      const session = event.sessions[i];
+      const sTime = `${session.date} | ${session.start} - ${session.end} `;
+      const sPrice = session.price ? session.price : `${session.priceMin} to ${session.priceMax}`
+      html += `<li>${sTime} <a href="${session.url}" target="new" class="button">Book on Eventbrite (${sPrice})</a></li>`
+    }
+    html +=     `</ul>`
+    html +=   `<div>`
+    html += '</article>'
+  })
   return html;
 }
+
 
 // run main immediately
 (async () => {
