a695fd60ba565f79c26922b60623b639d0e3fac7
Alex <alex@monney.me>
2025-07-06 17:10:31 +0100

    Add children's work gallery

--- src/controller/gallery.js
+++ src/controller/gallery.js
@@ -8,14 +8,21 @@ const appRoot = path.resolve(__dirname, '../..')
   , docsRoot = path.join(appRoot, 'docs')
 const config = require(path.resolve(appRoot, 'config.json'))
 
-// workshop images
-const images = fs.readdirSync(path.join(docsRoot, config.galleryPaths.gallery), { withFileTypes : true })
-  .filter(item => !item.isDirectory())
-  .filter(item => !item.name.match(/^\./)) // filter out dot files
-  .reverse() // put newest images first
-  .map(item => item.name)
+const getImages = dirPath => {
+  return fs.readdirSync(path.join(docsRoot, dirPath), { withFileTypes : true })
+    .filter(item => !item.isDirectory())
+    .filter(item => !item.name.match(/^\./)) // filter out dot files
+    .reverse() // put newest images first
+    .map(item => item.name)
+}
 
 module.exports = {
-  galleryImages : images,
-  baseURL : config.galleryPaths.gallery
+  childrensArtImages : {
+    baseURL : config.galleryPaths.childrensArtImages,
+    images : getImages(config.galleryPaths.childrensArtImages)
+  },
+  galleryImages : {
+    baseURL : config.galleryPaths.gallery,
+    images : getImages(config.galleryPaths.gallery)
+  }
 }
